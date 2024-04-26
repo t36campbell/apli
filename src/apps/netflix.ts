4949960cc89efeb762014177683b0b2c2cf5d307
t36campbell <campbell3636@yahoo.com>
2024-04-26 13:30:51 -0400

    created app stubs

---
+++ src/apps/netflix.ts
@@ -0,0 +1,15 @@
+import { playwright } from '../playwright';
+import { utils } from '../utils';
+
+export const netlfix = async (url: string) => {
+  const { browser, context, page } = await playwright.setup();
+
+  await page.goto(url);
+
+  let submit = await page.getByRole('button', { name: /Submit/ });
+
+  // Wait for submit
+  await submit.evaluate(playwright.clickListener).catch(utils.logError);
+
+  playwright.teardown(browser, context);
+};
